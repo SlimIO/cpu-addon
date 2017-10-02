4339ef039245426dba439a51c0e875b08e6c08c7
fraxken <gentilhomme.thomas@gmail.com>
2017-10-02 05:48:14 +0200

    Add eslint-plugin-slimio

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+// Require Node.JS Dep
+const os = require('os');
+const cpus = os.cpus();
+
 /**
  * @const CPU
  * @type {Addon}
@@ -18,7 +22,27 @@ async function get_info() {
  * @function main
  */
 async function main({ logger }) {
-    logger.info('CPU Run!');
+    //console.time('handle_cpu');
+    logger.info('CPU is running...');
+    const CPUQoS = [];
+    let total = 0, type;
+
+    for(let i = 0; i < cpus.length; i++) {
+        for(type in cpus[i].times) {
+            total += cpus[i].times[type];
+        }
+
+        for(type in cpus[i].times) {
+            // CPUQoS.push({
+            //     cpu_id: i,
+            //     type, 
+            //     value: Math.round(100 * cpus[i].times[type] / total),
+            //     timestamp: Date.now()
+            // });
+        }
+        total = 0;
+    }
+    //console.timeEnd('handle_cpu');
 }
 
 // Export addon
