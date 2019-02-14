43bf317217da19efbcb0ef7a8cb44208c4e3ac5c
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2019-02-14 13:05:39 +0100

    chore: refactor with new Metrics API

--- index.js
+++ index.js
@@ -3,24 +3,53 @@ const os = require("os");
 
 // Require SlimIO Dependencies
 const Units = require("@slimio/units");
-const Metrics = require("@slimio/metrics");
+const metrics = require("@slimio/metrics");
 const Addon = require("@slimio/addon");
 const Timer = require("@slimio/timer");
 
 // CONSTANTS
 const INTERVAL_MS = 5000;
 
-// Declare Addon
-const CPU = new Addon("CPU").lockOn("events");
-const Metric = new Metrics(CPU);
-
 /** @type {Number} */
 let intervalId;
 
-const pourcentCPU = {
-    idle: 0,
-    total: 0
-};
+// Declare Addon
+const CPU = new Addon("CPU").lockOn("events");
+const { Entity, MetricIdentityCard, Global: E_GLOBAL } = metrics(CPU);
+
+function rawPublish(micName, value, harvestedAt) {
+    if (!E_GLOBAL.mics.has(micName)) {
+        return void 0;
+    }
+    const mic = E_GLOBAL.mics.get(micName);
+
+    mic.publish(value, harvestedAt);
+
+    return void 0;
+}
+
+// Declare Entities and MIC
+{
+    const CPU_E = new Entity("CPU", {
+        description: "Central Processing Unit"
+    });
+    new MetricIdentityCard("TOTAL", { unit: Units.Pourcent, entity: CPU_E });
+
+    const cpus = os.cpus();
+    for (let id = 0; id < cpus.length; id++) {
+        const entity = new Entity(`CPU.${id}`, { parent: CPU_E })
+            .set("speed", cpus[id].speed)
+            .set("model", cpus[id].model);
+
+        // All Identity Card are Prefixed by the Identity Name (ex: CPU_USER).
+        const options = { unit: Units.MilliSecond, entity };
+        new MetricIdentityCard("USER", options);
+        new MetricIdentityCard("NICE", options);
+        new MetricIdentityCard("SYS", options);
+        new MetricIdentityCard("IDLE", options);
+        new MetricIdentityCard("IRQ", options);
+    }
+}
 
 /**
  * @func cpuInterval
@@ -28,85 +57,32 @@ const pourcentCPU = {
  * @returns {void}
  */
 function cpuInterval() {
+    const raw = { user: 0, nice: 0, sys: 0, idle: 0, irq: 0 };
     const harvestedAt = Date.now();
 
-    let user = 0;
-    let nice = 0;
-    let sys = 0;
-    let idle = 0;
-    let irq = 0;
-    let total = 0;
-
     const cpus = os.cpus();
     for (let id = 0; id < cpus.length; id++) {
-        Metric.publish(`CPU.${id}_USER`, cpus[id].times.user, harvestedAt);
-        Metric.publish(`CPU.${id}_NICE`, cpus[id].times.nice, harvestedAt);
-        Metric.publish(`CPU.${id}_SYS`, cpus[id].times.sys, harvestedAt);
-        Metric.publish(`CPU.${id}_IDLE`, cpus[id].times.idle, harvestedAt);
-        Metric.publish(`CPU.${id}_IRQ`, cpus[id].times.irq, harvestedAt);
+        const { user, nice, sys, idle, irq } = cpus[id].times;
+        rawPublish(`CPU.${id}_USER`, user, harvestedAt);
+        rawPublish(`CPU.${id}_NICE`, nice, harvestedAt);
+        rawPublish(`CPU.${id}_SYS`, sys, harvestedAt);
+        rawPublish(`CPU.${id}_IDLE`, idle, harvestedAt);
+        rawPublish(`CPU.${id}_IRQ`, irq, harvestedAt);
 
-        user += cpus[id].times.user;
-        nice += cpus[id].times.nice;
-        sys += cpus[id].times.sys;
-        idle += cpus[id].times.idle;
-        irq += cpus[id].times.irq;
+        raw.user += user;
+        raw.nice += nice;
+        raw.sys += sys;
+        raw.idle += idle;
+        raw.irq += irq;
     }
 
-    total = user + nice + sys + idle + irq;
-
-    const diffIdle = idle - pourcentCPU.idle;
-    const diffTotal = total - pourcentCPU.total;
-    const pourcent = idle / total;
-    Metric.publish("CPU_total", (1 - pourcent) * 100, harvestedAt);
-    Reflect.set(pourcentCPU, "idle", idle);
-    Reflect.set(pourcentCPU, "total", total);
+    const pourcent = raw.idle / (raw.user + raw.nice + raw.sys + raw.idle + raw.irq);
+    rawPublish("CPU_total", (1 - pourcent) * 100, harvestedAt);
 }
 
-CPU.of("Addon.ready", (addonName) => {
-    if (addonName === "events") {
-        CPU.ready();
-    }
-});
-
 // Triggered when the addon is started by the core
-CPU.on("start", () => {
-    const parent = Metric.entity("CPU", {
-        description: "Central Processing Unit"
-    });
-    Metric.identityCard("total", { unit: Units.Pourcent, entity: parent });
-
-    let user = 0;
-    let nice = 0;
-    let sys = 0;
-    let idle = 0;
-    let irq = 0;
-    let total = 0;
-
-    const cpus = os.cpus();
-    for (let id = 0; id < cpus.length; id++) {
-        const entity = Metric.entity(`CPU.${id}`, { parent })
-            .set("speed", cpus[id].speed)
-            .set("model", cpus[id].model);
-
-        // All Identity Card are Prefixed by the Identity Name (ex: CPU_USER).
-        const cardConfig = { unit: Units.MilliSecond, entity };
-        Metric.identityCard("USER", cardConfig);
-        Metric.identityCard("NICE", cardConfig);
-        Metric.identityCard("SYS", cardConfig);
-        Metric.identityCard("IDLE", cardConfig);
-        Metric.identityCard("IRQ", cardConfig);
-
-        user += cpus[id].times.user;
-        nice += cpus[id].times.nice;
-        sys += cpus[id].times.sys;
-        idle += cpus[id].times.idle;
-        irq += cpus[id].times.irq;
-    }
-
-    total = user + nice + sys + idle + irq;
-    Reflect.set(pourcentCPU, "idle", idle);
-    Reflect.set(pourcentCPU, "total", total);
-
+CPU.on("awake", () => {
+    CPU.ready();
     intervalId = Timer.setInterval(cpuInterval, INTERVAL_MS);
 });
 
